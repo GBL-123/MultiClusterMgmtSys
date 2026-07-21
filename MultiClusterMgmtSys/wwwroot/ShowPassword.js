cb2990142901d3c63017ec573564c736dd79c67d
GBL-123 <614373837@qq.com>
2026-07-22 00:44:33 +0800

    重构登录页面

---
+++ MultiClusterMgmtSys/wwwroot/ShowPassword.js
@@ -0,0 +1,18 @@
+﻿const passwordTimeouts = new WeakMap();
+
+function showPassword(inputElement) {
+    if (!inputElement) return;
+    const existingTimeout = passwordTimeouts.get(inputElement);
+    if (existingTimeout) clearTimeout(existingTimeout);
+    if (inputElement.type === 'password') {
+        inputElement.type = 'text';
+        const timeoutId = setTimeout(function () {
+            inputElement.type = 'password';
+            passwordTimeouts.delete(inputElement);
+        }, 5000);
+        passwordTimeouts.set(inputElement, timeoutId);
+    } else {
+        inputElement.type = 'password';
+        passwordTimeouts.delete(inputElement);
+    }
+}
