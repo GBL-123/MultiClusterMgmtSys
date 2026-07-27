42936847e521c90736b83c5aa4a77bc5f805fdf5
GBL-123 <614373837@qq.com>
2026-07-28 01:58:21 +0800

    修改布局

--- MultiClusterMgmtSys/Components/Clusters/ViewModels/ClusterViewModel.cs
+++ MultiClusterMgmtSys/Components/Clusters/ViewModels/ClusterViewModel.cs
@@ -5,15 +5,26 @@ namespace MultiClusterMgmtSys.Components.Clusters.ViewModels;
 public class ClusterViewModel
 {
     public int Id { get; set; }
+
     public string Name { get; set; } = "";
+
     public ClusterStatus Status { get; set; }
+
     public string StatusText { get; set; } = "";
+
     public string? Version { get; set; }
+
     public int NodeCount { get; set; }
+
     public int? GroupId { get; set; }
+
     public string? GroupName { get; set; }
+
     public string? ApiServer { get; set; }
+
     public DateTime CreatedAt { get; set; }
+
     public DateTime? LastCheckedAt { get; set; }
+
     public ConnectionType? ConnectionType { get; set; }
 }
